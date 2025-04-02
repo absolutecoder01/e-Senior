0dec00831125d45dced9fc51fb6ccb65b04efed1
absolutecoder01 <absolute.coder01@gmail.com>
2025-04-02 08:24:34 +0200

    Files

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,8 +9,8 @@ export function Hero() {
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span className="block">Witaj w </span>
             <span className="block text-blue-600">
-                <img src="src/images/eSeniorLogo_trim.png" className="inline align-middle w-20 h-22 mb-1 mr-0 pr-0" alt="Blue-Tinted Image"/>
-              <img src="src/images/minus.png" className="inline w-10 h-25 ml-0 pl-0" alt="Minus" />Senior</span>
+                <img src="public/eSeniorLogo_trim.png" className="inline align-middle w-20 h-22 mb-1 mr-0 pr-0" alt="Blue-Tinted Image"/>
+              <img src="public/minus.png" className="inline w-10 h-25 ml-0 pl-0" alt="Minus" />Senior</span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
             Twój przyjazny przewodnik po technologii.
